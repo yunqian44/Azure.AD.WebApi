7d2ecc49741183c8e03acc489fcffd8cf5f66911
allentmater <shenyafang8@outlook.com>
2020-05-11 14:26:54 +0800

    add swagger service open swagger middleware

--- Azure.AD.WebApi/Startup.cs
+++ Azure.AD.WebApi/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace Azure.AD.WebApi
 {
@@ -34,6 +36,28 @@ namespace Azure.AD.WebApi
 
             services.AddAuthentication(AzureADDefaults.JwtBearerAuthenticationScheme)
                .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "JWT授权(数据将在请求头中进行传输) 直接在下框中输入Bearer {token}（注意两者之间是一个空格）\"",
+                    Type = SecuritySchemeType.OAuth2,
+                    In = ParameterLocation.Header,//jwt默认存放Authorization信息的位置(请求头中)
+                    Flows = new OpenApiOAuthFlows()
+                    {
+                        Implicit = new OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri($"https://login.chinacloudapi.cn/{ Appsettings.app(new string[] { "AzureAD", "TenantId" })}/oauth2/authorize")
+
+                        }
+                    }
+                });
+                // 在header中添加token，传递到后台
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+
             services.AddControllers();
         }
 
@@ -44,6 +68,23 @@ namespace Azure.AD.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                //根据版本名称倒序 遍历展示
+                var ApiName = Appsettings.app(new string[] { "Startup", "ApiName" });
+                c.SwaggerEndpoint($"/swagger/v1/swagger.json", $"{ApiName} v1");
+
+                c.OAuthClientId(Appsettings.app(new string[] { "Swagger", "ClientId" }));
+                //c.OAuthClientSecret(Appsettings.app(new string[] { "Swagger", "ClientSecret" }));
+                c.OAuthRealm(Appsettings.app(new string[] { "AzureAD", "ClientId" }));
+                c.OAuthAppName("My API V1");
+                c.OAuthScopeSeparator(" ");
+                c.OAuthAdditionalQueryStringParams(new Dictionary<string, string>() { { "resource", Appsettings.app(new string[] { "AzureAD", "ClientId" }) } });
+            });
+            #endregion
 
             // open authentication middleware
             app.UseAuthentication();
