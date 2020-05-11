febf46acc613bec840c176cd320a49dfe4dcae95
allentmater <shenyafang8@outlook.com>
2020-05-11 14:12:05 +0800

    add Azure AD Authentication Service open authentication middleware

--- Azure.AD.WebApi/Startup.cs
+++ Azure.AD.WebApi/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +26,8 @@ namespace Azure.AD.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(AzureADDefaults.JwtBearerAuthenticationScheme)
+               .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
             services.AddControllers();
         }
 
@@ -34,6 +38,10 @@ namespace Azure.AD.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // open authentication middleware
+            app.UseAuthentication();
+
 
             app.UseRouting();
 
